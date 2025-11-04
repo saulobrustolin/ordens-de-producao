3d6ac13dcce758263f42bc79270af1e320b12c37
Saulo Brustolin <saulo.brustolin@ampla.ind.br>
2025-11-04 14:55:53 -0300

    adding OrderModule in App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from '../auth/auth.module';
+import { OrdersModule } from 'src/orders/orders.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, OrdersModule],
   controllers: [AppController],
   providers: [AppService],
 })
