0014cf559d92fc6fa0207c595ed9f08ae0f4c38e
Saulo Brustolin <saulo.brustolin@ampla.ind.br>
2025-11-05 15:46:45 -0300

    adding Swapper/Open API in project

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('API de Ordens')
+    .setDescription('Documentação da API de Autenticação, Geração de pedidos e Geração de passos')
+    .setVersion('1.0.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }))
   await app.listen(process.env.PORT ?? 7778);
 }
