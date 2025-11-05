9d4ae22814aa383248b272e97a108455b5f1b914
Saulo Brustolin <saulo.brustolin@ampla.ind.br>
2025-11-05 10:11:10 -0300

    adding UsersModule in App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppService } from './app.service';
 import { AuthModule } from '../auth/auth.module';
 import { OrdersModule } from 'src/orders/orders.module';
 import { StepsModule } from 'src/order-steps/order-steps.module';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
-  imports: [AuthModule, OrdersModule, StepsModule],
+  imports: [AuthModule, OrdersModule, StepsModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
