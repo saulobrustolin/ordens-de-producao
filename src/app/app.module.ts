ad88ae24cbf9c3a87b485135d65cefdda8b7c292
Saulo Brustolin <saulo.brustolin@ampla.ind.br>
2025-11-05 10:19:24 -0300

    adding JWT provider in App global

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,18 @@ import { StepsModule } from 'src/order-steps/order-steps.module';
 import { UsersModule } from 'src/users/users.module';
 import { RolesGuard } from 'src/auth/guards/roles.guard';
 import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from '@nestjs/passport';
 
 @Module({
   imports: [AuthModule, OrdersModule, StepsModule, UsersModule],
   controllers: [AppController],
   providers: [
-    AppService, {
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard('jwt')
+    },
+    {
       provide: APP_GUARD,
       useClass: RolesGuard
     }
