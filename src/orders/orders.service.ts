93bcb926993960b89c8e94dbdb4c86581d84f17c
Saulo Brustolin <saulo.brustolin@ampla.ind.br>
2025-11-05 08:19:06 -0300

    update path to prisma service

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@nestjs/common";
-import { PrismaService } from "src/prisma/prisma.service";
+import { PrismaService } from "../prisma/prisma.service";
 import { CreateOrder } from "./dto/create-order.dto";
 import { UpdateOrder } from "./dto/update-order.dto";
 
